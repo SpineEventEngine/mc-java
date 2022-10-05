3bf9cc72cf6955a09ac085f56103e8d4bcc24973
Alex Tymchenko <alex.tymchenko@teamdev.com>
2022-10-05 14:25:36 +0300

    Enable `zip64` extension, as the resulting JAR got too big.

--- mc-java-protoc/build.gradle.kts
+++ mc-java-protoc/build.gradle.kts
@@ -68,6 +68,7 @@ tasks.jar {
     // We should provide a classifier or else Protobuf Gradle plugin will substitute it with
     // an OS-specific one.
     archiveClassifier.set("exe")
+    setZip64(true)  /* The archive has way too many items. So using the Zip64 mode. */
 
     duplicatesStrategy = DuplicatesStrategy.INCLUDE
 }
