a4cc5e7194ae687f5555918997718a5125f2e593
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-10-10 23:50:47 +0300

    Fix import

--- tests/validation/src/test/java/io/spine/test/protobuf/GeneratedMessageTest.java
+++ tests/validation/src/test/java/io/spine/test/protobuf/GeneratedMessageTest.java
@@ -31,7 +31,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static com.google.common.truth.Truth.assertThat;
-import static io.spine.validate.Validate.checkValid;
+import static io.spine.validate.Validate.check;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 @DisplayName("Generated message should")
