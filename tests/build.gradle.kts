51f817d2b9477f51d8b991f2e961f27797f5163c
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2022-10-10 23:25:03 +0300

    Add dependency on validation runtime

--- tests/build.gradle.kts
+++ tests/build.gradle.kts
@@ -110,6 +110,7 @@ allprojects {
     version = extra["versionToPublish"]!!
 
     val baseVersion: String by extra
+    val validationVersion: String by extra
     val toolBaseVersion: String by extra
     val timeVersion: String by extra
     configurations {
@@ -163,6 +164,7 @@ subprojects {
         errorproneJavac(ErrorProne.javacPlugin)
         ErrorProne.annotations.forEach { compileOnly(it) }
         implementation("io.spine:spine-base:$baseVersion")
+        implementation("io.spine.validation:spine-validation-java-runtime:$validationVersion")
         testImplementation("io.spine.tools:spine-testlib:$baseVersion")
         Truth.libs.forEach { testImplementation(it) }
         testRuntimeOnly(JUnit.runner)
