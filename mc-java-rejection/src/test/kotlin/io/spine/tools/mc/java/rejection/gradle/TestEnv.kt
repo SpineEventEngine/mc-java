f74f2459d98748b1e1e8786f83704431d641cdc1
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-01-20 17:40:43 +0000

    Use div operators for paths

--- mc-java-rejection/src/test/kotlin/io/spine/tools/mc/java/rejection/gradle/TestEnv.kt
+++ mc-java-rejection/src/test/kotlin/io/spine/tools/mc/java/rejection/gradle/TestEnv.kt
@@ -27,11 +27,14 @@ package io.spine.tools.mc.java.rejection.gradle
 
 import io.spine.code.java.PackageName
 import io.spine.code.proto.FieldName
-import io.spine.tools.code.SourceSetName.Companion.main
-import io.spine.tools.java.fs.DefaultJavaPaths
+import io.spine.tools.div
+import io.spine.tools.fs.DirectoryName.generated
+import io.spine.tools.fs.DirectoryName.main
+import io.spine.tools.fs.DirectoryName.spine
 import io.spine.tools.java.fs.FileName
 import io.spine.tools.java.fs.toDirectory
 import java.nio.file.Path
+import kotlin.io.path.div
 
 internal object TestEnv {
 
@@ -46,12 +49,7 @@ internal object TestEnv {
 
     fun rejectionsJavadocThrowableSource(projectDir: Path): Path {
         val javaPackage = JAVA_PACKAGE.toDirectory()
-        return DefaultJavaPaths.at(projectDir)
-            .generated()
-            .spine(main)
-            .path()
-            .resolve(javaPackage)
-            .resolve(REJECTION_FILE_NAME.value())
+        return projectDir / generated / main / spine / javaPackage / REJECTION_FILE_NAME.value()
     }
 
     fun rejectionWithJavadoc(): Iterable<String> {
