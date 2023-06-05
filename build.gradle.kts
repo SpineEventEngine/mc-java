ad81baf9b5de6961bbd9225d4bc36ebd3eff9daa
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-06-05 17:51:50 +0100

    Remove commented out code

--- build.gradle.kts
+++ build.gradle.kts
@@ -28,6 +28,7 @@
 
 import io.spine.internal.dependency.Protobuf
 import io.spine.internal.dependency.Spine
+import io.spine.internal.dependency.Validation
 import io.spine.internal.gradle.RunBuild
 import io.spine.internal.gradle.publish.PublishingRepos
 import io.spine.internal.gradle.publish.SpinePublishing
@@ -44,15 +45,15 @@ buildscript {
     standardSpineSdkRepositories()
 
     val spine = io.spine.internal.dependency.Spine
-    io.spine.internal.gradle.doForceVersions(configurations)
+    doForceVersions(configurations)
     configurations {
         all {
             resolutionStrategy {
                 force(
                     spine.base,
+                    spine.logging,
                     spine.toolBase,
                     spine.server,
-                    spine.logging,
                     io.spine.internal.dependency.Validation.runtime,
                     io.spine.internal.dependency.ProtoData.pluginLib
                 )
@@ -98,7 +99,7 @@ subprojects {
     apply(plugin = "module")
     apply(plugin = "io.spine.protodata")
     dependencies {
-        protoData(Spine.validation.java)
+        protoData(Validation.java)
     }
     setupCodegen()
 }
@@ -164,7 +165,6 @@ typealias Module = Project
 fun Module.setupCodegen() {
 
     protobuf {
-        //generatedFilesBaseDir = "$projectDir/generated"
         protoc { artifact = Protobuf.compiler }
     }
 
@@ -172,36 +172,10 @@ fun Module.setupCodegen() {
         renderers(
             "io.spine.validation.java.PrintValidationInsertionPoints",
             "io.spine.validation.java.JavaValidationRenderer",
-
-            // Suppress warnings in the generated code.
-            "io.spine.protodata.codegen.java.file.PrintBeforePrimaryDeclaration",
-            "io.spine.protodata.codegen.java.annotation.SuppressWarningsAnnotation"
         )
         plugins(
             "io.spine.validation.ValidationPlugin"
         )
-    }
-
-    val generatedSourceProto = "$buildDir/generated/source/proto"
-
-    /**
-     * Remove the generated vanilla proto code.
-     */
-    project.afterEvaluate {
-        val generatedSourceProtoDir = File(generatedSourceProto)
-        val notInSourceDir: (File) -> Boolean = { file -> !file.residesIn(generatedSourceProtoDir) }
-
-        tasks.withType<JavaCompile>().forEach {
-            it.source = it.source.filter(notInSourceDir).asFileTree
-        }
-
-        tasks.withType<KotlinCompile<*>>().forEach {
-            val thisTask = it as KotlinCompileTool
-            val filteredKotlin = thisTask.sources.filter(notInSourceDir).toSet()
-            with(thisTask.sources as ConfigurableFileCollection) {
-                setFrom(filteredKotlin)
-            }
-        }
     }
 }
 
