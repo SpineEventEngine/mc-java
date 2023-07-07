2f76073d02b8339a9b6cb76af158e2b3e0b14cdc
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-07-07 16:09:50 +0100

    Remove redundant annotation

--- mc-java/src/main/kotlin/io/spine/tools/mc/java/gradle/Artifacts.kt
+++ mc-java/src/main/kotlin/io/spine/tools/mc/java/gradle/Artifacts.kt
@@ -1,5 +1,5 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2023, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -134,7 +134,6 @@ private val validationVersion: String by lazy {
 /**
  * The Maven artifact containing the `spine-validation-java-extensions` module.
  */
-@Suppress("FunctionOnlyReturningConstant") // make detekt happy about the `getName()`
 @get:JvmName("validationJavaBundle")
 internal val validationJavaBundle: Artifact by lazy {
     artifact {
