772a561d7e4404846dedaf373fb319c6c8629d06
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-07-08 20:06:15 +0100

    Improve method name

--- mc-java-rejection/src/main/kotlin/io/spine/tools/mc/java/rejection/Methods.kt
+++ mc-java-rejection/src/main/kotlin/io/spine/tools/mc/java/rejection/Methods.kt
@@ -23,7 +23,6 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
-
 package io.spine.tools.mc.java.rejection
 
 import com.google.errorprone.annotations.Immutable
