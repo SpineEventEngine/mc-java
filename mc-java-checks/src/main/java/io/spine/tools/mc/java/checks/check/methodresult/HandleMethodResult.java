00a4bc7903d6d0fa9ce368060d1d3f4568e4dd1c
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-07-25 02:34:25 +0100

    Address PMD error
    Also:
     * Improve code layout.

--- mc-java-checks/src/main/java/io/spine/tools/mc/java/checks/check/methodresult/HandleMethodResult.java
+++ mc-java-checks/src/main/java/io/spine/tools/mc/java/checks/check/methodresult/HandleMethodResult.java
@@ -27,7 +27,6 @@
 package io.spine.tools.mc.java.checks.check.methodresult;
 
 import com.google.auto.service.AutoService;
-import com.google.common.collect.ImmutableList;
 import com.google.errorprone.BugPattern;
 import com.google.errorprone.ErrorProneFlags;
 import com.google.errorprone.VisitorState;
@@ -37,6 +36,7 @@ import com.google.errorprone.bugpatterns.CheckReturnValue;
 import com.google.errorprone.bugpatterns.checkreturnvalue.PackagesRule;
 import com.google.errorprone.bugpatterns.checkreturnvalue.ResultUsePolicy;
 import com.google.errorprone.bugpatterns.checkreturnvalue.ResultUsePolicyEvaluator;
+import com.google.errorprone.bugpatterns.checkreturnvalue.ResultUsePolicyEvaluator.MethodInfo;
 import com.google.errorprone.bugpatterns.checkreturnvalue.ResultUseRule;
 import com.google.errorprone.bugpatterns.threadsafety.ConstantExpressions;
 import com.google.errorprone.matchers.Matcher;
@@ -74,8 +74,8 @@ import static com.google.errorprone.util.ASTHelpers.enclosingElements;
  * An ErrorProne check which ensures that the values returned from methods are not
  * accidentally ignored.
  *
- * <p>This check is a substitute for {@link CheckReturnValue}. The difference is that this check
- * ignores invocations of mutating methods on message builders.
+ * <p>This check is a substitute for {@link CheckReturnValue}.
+ * The difference is that this check ignores invocations of mutating methods on message builders.
  *
  * <p>The check may be suppressed in the same ways as {@link CheckReturnValue}.
  */
@@ -96,26 +96,27 @@ public final class HandleMethodResult extends AbstractReturnValueIgnored {
 
     private static final String CHECK_RETURN_VALUE = "CheckReturnValue";
     private static final String CAN_IGNORE_RETURN_VALUE = "CanIgnoreReturnValue";
-    private static final ImmutableList<String> MUTUALLY_EXCLUSIVE_ANNOTATIONS =
-            ImmutableList.of(CHECK_RETURN_VALUE, CAN_IGNORE_RETURN_VALUE);
 
     public static final String CHECK_ALL_CONSTRUCTORS = "CheckReturnValue:CheckAllConstructors";
     public static final String CHECK_ALL_METHODS = "CheckReturnValue:CheckAllMethods";
 
     static final String CRV_PACKAGES = "CheckReturnValue:Packages";
 
-    private static final ResultUsePolicyEvaluator.MethodInfo<VisitorState, Symbol, MethodSymbol> METHOD_INFO =
-            new ResultUsePolicyEvaluator.MethodInfo<>() {
+    private static final MethodInfo<VisitorState, Symbol, MethodSymbol> METHOD_INFO =
+            new MethodInfo<>() {
                 @Override
                 public Stream<Symbol> scopeMembers(
                         ResultUseRule.RuleScope scope, MethodSymbol method, VisitorState context) {
                     switch (scope) {
                         case ENCLOSING_ELEMENTS:
                             return enclosingElements(method)
-                                    .filter(s -> s instanceof ClassSymbol || s instanceof PackageSymbol);
+                                    .filter(s -> s instanceof ClassSymbol
+                                                 || s instanceof PackageSymbol);
                         case GLOBAL:
                         case METHOD:
                             return Stream.of(method);
+                        default:
+                            break;
                     }
                     throw new AssertionError(scope);
                 }
@@ -133,48 +134,48 @@ public final class HandleMethodResult extends AbstractReturnValueIgnored {
                 }
             };
 
+    @SuppressWarnings("NonSerializableFieldInSerializableClass")
     private final ResultUsePolicyEvaluator<VisitorState, Symbol, Symbol.MethodSymbol> evaluator;
 
     public HandleMethodResult() {
         super(ConstantExpressions.fromFlags(ErrorProneFlags.empty()));
 
-        var builder =
-                ResultUsePolicyEvaluator.builder(METHOD_INFO)
-                        .addRules(
-                                // The order of these rules matters somewhat because when checking a method, we'll
-                                // evaluate them in the order they're listed here and stop as soon as one of them
-                                // returns a result. The order shouldn't matter because most of these, with the
-                                // exception of perhaps the external ignore list, are equivalent in importance and
-                                // we should be checking declarations to ensure they aren't producing differing
-                                // results (i.e. ensuring an @AutoValue.Builder setter method isn't annotated @CRV).
-                                mapAnnotationSimpleName(CHECK_RETURN_VALUE, EXPECTED),
-                                mapAnnotationSimpleName(CAN_IGNORE_RETURN_VALUE, OPTIONAL),
-                                protoBuilders(),
-                                mutableProtos(),
-                                autoValues(),
-                                autoValueBuilders(),
-                                autoBuilders(),
+        var builder = ResultUsePolicyEvaluator.builder(METHOD_INFO).addRules(
+                // The order of these rules matters somewhat because when checking a method, we'll
+                // evaluate them in the order they're listed here and stop as soon as one of them
+                // returns a result. The order shouldn't matter because most of these, except
+                // perhaps the external ignore list, are equivalent in importance, and
+                // we should be checking declarations to ensure they aren't producing differing
+                // results (i.e. ensuring an @AutoValue.Builder setter method isn't annotated @CRV).
+                mapAnnotationSimpleName(CHECK_RETURN_VALUE, EXPECTED),
+                mapAnnotationSimpleName(CAN_IGNORE_RETURN_VALUE, OPTIONAL),
+                protoBuilders(),
+                mutableProtos(),
+                autoValues(),
+                autoValueBuilders(),
+                autoBuilders(),
 
-                                // This is conceptually lower precedence than the above rules.
-                                externalIgnoreList());
+                // This is conceptually lower precedence than the above rules.
+                externalIgnoreList()
+        );
 
         var flags = ErrorProneFlags.empty();
-        flags
-                .getList(CRV_PACKAGES)
-                .ifPresent(packagePatterns -> builder.addRule(PackagesRule.fromPatterns(packagePatterns)));
-        this.evaluator =
-                builder
-                        .addRule(
-                                globalDefault(
-                                        defaultPolicy(flags, CHECK_ALL_METHODS),
-                                        defaultPolicy(flags, CHECK_ALL_CONSTRUCTORS)))
-                        .build();
+        flags.getList(CRV_PACKAGES)
+             .ifPresent(packagePatterns -> builder.addRule(
+                     PackagesRule.fromPatterns(packagePatterns)));
+        this.evaluator = builder
+                .addRule(
+                        globalDefault(
+                                defaultPolicy(flags, CHECK_ALL_METHODS),
+                                defaultPolicy(flags, CHECK_ALL_CONSTRUCTORS))
+                )
+                .build();
     }
 
     @Override
     public Matcher<ExpressionTree> specializedMatcher() {
         Matcher<ExpressionTree> checkReturnValue =
-                (tree, state) -> getMethodPolicy(tree, state) == ResultUsePolicy.EXPECTED;
+                (tree, state) -> getMethodPolicy(tree, state) == EXPECTED;
 
         var notBuilderSetter = not(builderSetter());
         return allOf(checkReturnValue, notBuilderSetter);
@@ -196,7 +197,9 @@ public final class HandleMethodResult extends AbstractReturnValueIgnored {
 
     private static Optional<Symbol.MethodSymbol> methodSymbol(ExpressionTree tree) {
         var sym = ASTHelpers.getSymbol(tree);
-        return sym instanceof Symbol.MethodSymbol ? Optional.of((Symbol.MethodSymbol) sym) : Optional.empty();
+        return sym instanceof Symbol.MethodSymbol
+               ? Optional.of((Symbol.MethodSymbol) sym)
+               : Optional.empty();
     }
 
     private static Optional<ResultUsePolicy> defaultPolicy(ErrorProneFlags flags, String flag) {
