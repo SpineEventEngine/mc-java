92d9b7d8b05bd2081f2e41fd86b7677a2b597462
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-11-18 01:06:29 +0000

    Convert test to Kotlin
    Also:
     * Remove outdated test.

--- mc-java-protoc/src/test/kotlin/io/spine/tools/mc/java/protoc/message/BuilderGenSpec.kt
+++ mc-java-protoc/src/test/kotlin/io/spine/tools/mc/java/protoc/message/BuilderGenSpec.kt
@@ -23,44 +23,32 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package io.spine.tools.mc.java.protoc.message
 
-package io.spine.tools.mc.java.protoc.message;
+import com.google.protobuf.compiler.PluginProtos
+import com.google.protobuf.compiler.codeGeneratorRequest
+import io.kotest.matchers.collections.shouldHaveSize
+import io.kotest.matchers.string.shouldNotBeEmpty
+import io.spine.test.tools.mc.java.protoc.BuilderTestProto
+import org.junit.jupiter.api.DisplayName
+import org.junit.jupiter.api.Test
 
-import com.google.protobuf.compiler.PluginProtos.CodeGeneratorRequest;
-import com.google.protobuf.compiler.PluginProtos.Version;
-import io.spine.test.tools.mc.java.protoc.BuilderTestProto;
-import io.spine.tools.mc.java.codegen.CodegenOptions;
-import io.spine.tools.mc.java.protoc.NoOpGenerator;
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
-
-import static com.google.common.truth.Truth.assertThat;
-
-@DisplayName("BuilderGenerator should")
-class BuilderGenTest {
+@DisplayName("`BuilderGen` should")
+internal class BuilderGenSpec {
 
     @Test
-    @DisplayName("produce builder insertion points")
-    void produceBuilderInsertionPoints() {
-        var generator = BuilderGen.instance(CodegenOptions.getDefaultInstance());
-        var file = BuilderTestProto.getDescriptor();
-        var request = CodeGeneratorRequest.newBuilder()
-                .addProtoFile(file.toProto())
-                .addFileToGenerate(file.getFullName())
-                .setCompilerVersion(Version.newBuilder().setMajor(3).build())
-                .build();
-        var response = generator.process(request);
-        var files = response.getFileList();
-        assertThat(files).hasSize(1);
-        assertThat(files.get(0).getInsertionPoint()).isNotEmpty();
-    }
+    fun `produce builder insertion points`() {
+        val generator = BuilderGen.instance()
+        val file = BuilderTestProto.getDescriptor()
+        val request = codeGeneratorRequest {
+            protoFile.add(file.toProto())
+            fileToGenerate.add(file.fullName)
+            compilerVersion = PluginProtos.Version.newBuilder().setMajor(3).build()
+        }
+        val response = generator.process(request)
+        val files = response.fileList
 
-    @Test
-    @DisplayName("do nothing if configured to skip validating builders")
-    void ignoreIfConfigured() {
-        var config = CodegenOptions.newBuilder();
-        config.getValidationBuilder().setSkipBuilders(true);
-        var generator = BuilderGen.instance(config.build());
-        assertThat(generator).isInstanceOf(NoOpGenerator.class);
+        files shouldHaveSize 1
+        files[0]!!.insertionPoint.shouldNotBeEmpty()
     }
 }
