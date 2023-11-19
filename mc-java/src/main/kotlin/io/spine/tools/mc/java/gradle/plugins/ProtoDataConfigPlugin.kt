ba6db3948c700183bb495c585fb616dea6566b3d
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-11-19 01:21:37 +0000

    Migrate `ProtoDataConfigPlugin` to Kotlin

--- mc-java/src/main/kotlin/io/spine/tools/mc/java/gradle/plugins/ProtoDataConfigPlugin.kt
+++ mc-java/src/main/kotlin/io/spine/tools/mc/java/gradle/plugins/ProtoDataConfigPlugin.kt
@@ -23,157 +23,151 @@
  * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
  * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
+package io.spine.tools.mc.java.gradle.plugins
 
-package io.spine.tools.mc.java.gradle.plugins;
-
-import com.google.common.collect.ImmutableList;
-import io.spine.protodata.gradle.CodegenSettings;
-import io.spine.protodata.gradle.plugin.LaunchProtoData;
-import io.spine.tools.gradle.Artifact;
-import io.spine.tools.mc.java.gradle.McJava;
-import io.spine.tools.mc.java.gradle.Validation;
-import org.checkerframework.checker.nullness.qual.Nullable;
-import org.gradle.api.Plugin;
-import org.gradle.api.Project;
-import org.gradle.api.artifacts.Dependency;
-
-import static io.spine.tools.fs.DirectoryName.kotlin;
-import static io.spine.tools.mc.java.gradle.Artifacts.toolBase;
-import static io.spine.tools.mc.java.gradle.Projects.getGeneratedGrpcDirName;
-import static io.spine.tools.mc.java.gradle.Projects.getGeneratedJavaDirName;
-import static io.spine.tools.mc.java.gradle.Projects.getMcJava;
-import static java.io.File.separatorChar;
-import static java.lang.String.format;
-import static java.util.Objects.requireNonNullElseGet;
+import io.spine.protodata.gradle.CodegenSettings
+import io.spine.protodata.gradle.plugin.LaunchProtoData
+import io.spine.tools.fs.DirectoryName
+import io.spine.tools.gradle.Artifact
+import io.spine.tools.mc.java.gradle.McJava.annotation
+import io.spine.tools.mc.java.gradle.McJava.base
+import io.spine.tools.mc.java.gradle.McJava.rejection
+import io.spine.tools.mc.java.gradle.Validation.javaCodegenBundle
+import io.spine.tools.mc.java.gradle.Validation.javaRuntime
+import io.spine.tools.mc.java.gradle.generatedGrpcDirName
+import io.spine.tools.mc.java.gradle.generatedJavaDirName
+import io.spine.tools.mc.java.gradle.mcJava
+import io.spine.tools.mc.java.gradle.toolBase
+import java.io.File
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.api.artifacts.Dependency
+import org.gradle.kotlin.dsl.getByType
+import org.gradle.kotlin.dsl.withType
 
 /**
  * The plugin that configures ProtoData for the associated project.
  *
- * <p>We use ProtoData and the Validation library to generate validation code right inside
- * the Protobuf message classes. This plugin applies the {@code io.spine.protodata} plugin,
+ *
+ * We use ProtoData and the Validation library to generate validation code right inside
+ * the Protobuf message classes. This plugin applies the `io.spine.protodata` plugin,
  * configures its extension, writes the ProtoData configuration file, and adds the required
  * dependencies to the target project.
  */
-final class ProtoDataConfigPlugin implements Plugin<Project> {
-
-    private static final String PROTO_DATA_ID = "io.spine.protodata";
-    private static final String CONFIG_SUBDIR = "protodata-config";
-
-    @SuppressWarnings("DuplicateStringLiteralInspection")
-        // Could be duplicated in auto-generated Gradle code via script plugins in `buildSrc`.
-    private static final String PROTODATA_CONFIGURATION = "protoData";
-    private static final String IMPL_CONFIGURATION = "implementation";
+internal class ProtoDataConfigPlugin : Plugin<Project> {
 
     /**
-     * Applies the {@code io.spine.protodata} plugin to the project and, if the user needs
+     * Applies the `io.spine.protodata` plugin to the project and, if the user needs
      * validation code generation, configures ProtoData to generate Java validation code.
      *
-     * <p>ProtoData configuration is a tricky operation because of Gradle's lifecycle.
-     * We need to squeeze our configuration before the {@code LaunchProtoData} task is configured.
-     * This means adding the {@code afterEvaluate(..)} hook before the ProtoData Gradle plugin
+     * ProtoData configuration is a tricky operation because of Gradle's lifecycle.
+     * We need to squeeze our configuration before the `LaunchProtoData` task is configured.
+     * This means adding the `afterEvaluate(..)` hook before the ProtoData Gradle plugin
      * is applied to the project.
      */
-    @Override
-    public void apply(Project project) {
-        project.afterEvaluate(ProtoDataConfigPlugin::configureProtoData);
-        project.getPluginManager()
-              .apply(PROTO_DATA_ID);
+    override fun apply(project: Project) {
+        project.afterEvaluate {
+            it.configureProtoData()
+        }
+        project.pluginManager.apply(PROTO_DATA_ID)
     }
 
-    private static void configureProtoData(Project project) {
-        configurePlugins(project);
+    companion object {
 
-        var tasks = project.getTasks();
-        tasks.withType(LaunchProtoData.class, task -> {
-            var name = task.getName();
-            var taskName = format("writeConfigFor_%s", name);
-            var configTask = tasks.create(
-                    taskName,
-                    GenerateProtoDataConfig.class,
-                    t -> linkConfigFile(project, task, t)
-            );
-            task.dependsOn(configTask);
-        });
+        /**
+         * The ID of the ProtoData Gradle Plugin.
+         */
+        private const val PROTO_DATA_ID = "io.spine.protodata"
+
+        /**
+         * The name of the directory where the ProtoData configuration files are stored.
+         */
+        private const val CONFIG_SUBDIR = "protodata-config"
+
+        /**
+         * The name of the `protoData` configuration.
+         */
+        private const val PROTODATA_CONFIGURATION = "protoData"
+
+        /**
+         * The name of the `implementation` configuration.
+         */
+        private const val IMPL_CONFIGURATION = "implementation"
+
+        fun configTaskName(launchTask: String): String = "writeConfigFor_${launchTask}"
     }
 
-    private static void addDependency(
-            Project project,
-            String configurationName,
-            Artifact artifact
-    ) {
-        var dependency = findDependency(project, artifact);
-        project.getDependencies()
-               .add(configurationName, requireNonNullElseGet(dependency, artifact::notation));
-    }
-
-    private static void addDependencies(
-            Project project,
-            String configurationName,
-            Artifact... artifacts
-    ) {
-        for (var artifact : artifacts) {
-            addDependency(project, configurationName, artifact);
+    private fun Project.configureProtoData() {
+        configurePlugins()
+        tasks.withType(LaunchProtoData::class.java) { task ->
+            val taskName = configTaskName(task.name)
+            val configTask = tasks.create(taskName, GenerateProtoDataConfig::class.java) {
+                t -> linkConfigFile(task, t)
+            }
+            task.dependsOn(configTask)
         }
     }
 
     /**
      * Configures ProtoData with plugins, for the given Gradle project.
      */
-    private static void configurePlugins(Project project) {
-        var codegen = project.getExtensions()
-                             .getByType(CodegenSettings.class);
-
-        configureValidationRendering(project, codegen);
+    private fun Project.configurePlugins() {
+        val codegen = extensions.getByType<CodegenSettings>()
+        configureValidationRendering(codegen)
 
         codegen.plugins(
-                "io.spine.tools.mc.java.rejection.RejectionPlugin"
-        );
+            "io.spine.tools.mc.java.rejection.RejectionPlugin"
+        )
 
-        codegen.setSubDirs(ImmutableList.of(
-                getGeneratedJavaDirName().value(),
-                getGeneratedGrpcDirName().value(),
-                kotlin.value()
-        ));
+        codegen.subDirs = listOf(
+            generatedJavaDirName.value(),
+            generatedGrpcDirName.value(),
+            DirectoryName.kotlin.value()
+        )
 
         addDependencies(
-                project, PROTODATA_CONFIGURATION,
-                McJava.base(),
-                McJava.annotation(),
-                McJava.rejection(),
-                toolBase()
-        );
+            PROTODATA_CONFIGURATION,
+            base,
+            annotation,
+            rejection,
+            toolBase
+        )
     }
 
-    private static void configureValidationRendering(Project project, CodegenSettings codegen) {
+    private fun Project.configureValidationRendering(codegen: CodegenSettings) {
         codegen.plugins(
-                "io.spine.validation.java.JavaValidationPlugin"
-        );
-        var version = getMcJava(project).codegen.validation().getVersion().get();
-        addDependency(project, PROTODATA_CONFIGURATION, Validation.javaCodegenBundle(version));
-        addDependency(project, IMPL_CONFIGURATION, Validation.javaRuntime(version));
+            "io.spine.validation.java.JavaValidationPlugin"
+        )
+        val version = mcJava.codegen.validation().version.get()
+        addDependency(PROTODATA_CONFIGURATION, javaCodegenBundle(version))
+        addDependency(IMPL_CONFIGURATION, javaRuntime(version))
     }
 
-    private static
-    void linkConfigFile(Project target, LaunchProtoData task, GenerateProtoDataConfig t) {
-        var targetFile = t.getTargetFile();
-        var fileName = t.getName() + ".bin";
-        var defaultFile = target.getLayout()
-                                .getBuildDirectory()
-                                .file(CONFIG_SUBDIR + separatorChar + fileName);
-        targetFile.convention(defaultFile);
-        task.getConfigurationFile()
-            .set(targetFile);
+    private fun Project.linkConfigFile(launch: LaunchProtoData, config: GenerateProtoDataConfig) {
+        val targetFile = config.targetFile
+        val fileName = config.name + ".bin"
+        val defaultFile = layout.buildDirectory.file(CONFIG_SUBDIR + File.separatorChar + fileName)
+        targetFile.convention(defaultFile)
+        launch.configurationFile.set(targetFile)
     }
+}
 
-    private static @Nullable Dependency findDependency(Project project, Artifact artifact) {
-        var dependencies = project.getConfigurations().stream()
-                .flatMap(c -> c.getDependencies().stream());
+private fun Project.addDependency(configurationName: String, artifact: Artifact) {
+    val dependency = findDependency(artifact) ?: artifact.notation()
+    dependencies.add(configurationName, dependency)
+}
 
-        var found = dependencies.filter((d) ->
-            d.getGroup() != null
-                    && d.getGroup().equals(artifact.group())
-                    && d.getName().equals(artifact.name())
-        ).findFirst().orElse(null);
-        return found;
+private fun Project.addDependencies(configurationName: String, vararg artifacts: Artifact) {
+    for (artifact in artifacts) {
+        addDependency(configurationName, artifact)
     }
+}
+
+private fun Project.findDependency(artifact: Artifact): Dependency? {
+    val dependencies = configurations.flatMap { c -> c.dependencies }
+    val found = dependencies.firstOrNull { d ->
+        d.group?.let { it == artifact.group() && d.name == artifact.name() }
+            ?: false
+    }
+    return found
 }
