14a197fe83d1673f3ab97dac42f0dd6785d66013
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-12-26 23:07:25 +0000

    Sync. `dogfoodingVersion`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoData.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoData.kt
@@ -74,7 +74,7 @@ object ProtoData {
      * transitional dependencies, this is the version used to build the project itself.
      */
     val dogfoodingVersion: String
-    private const val fallbackDfVersion = "0.15.2"
+    private const val fallbackDfVersion = "0.15.3"
 
     /**
      * The artifact for the ProtoData Gradle plugin.
