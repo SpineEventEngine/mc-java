3f4832d3217b3987723f9a652542ec801aed4641
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-12-26 22:40:43 +0000

    Fix adding option

--- mc-java-annotation/src/main/kotlin/io/spine/tools/mc/annotation/MessageAnnotationsView.kt
+++ mc-java-annotation/src/main/kotlin/io/spine/tools/mc/annotation/MessageAnnotationsView.kt
@@ -43,7 +43,7 @@ internal class MessageAnnotationsView :
 
     @Subscribe
     fun on(@External e: TypeOptionDiscovered) = alter {
-        optionList.add(e.option)
+        addOption(e.option)
     }
 
     @Subscribe
