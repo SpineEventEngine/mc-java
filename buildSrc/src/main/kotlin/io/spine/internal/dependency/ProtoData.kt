0746efab6b112978a1f89dfdbae0d12a341ec792
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2023-12-26 22:33:21 +0000

    Bump ProtoData and Validation

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoData.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/ProtoData.kt
@@ -65,7 +65,7 @@ object ProtoData {
      * The version of ProtoData dependencies.
      */
     val version: String
-    private const val fallbackVersion = "0.15.2"
+    private const val fallbackVersion = "0.15.3"
 
     /**
      * The distinct version of ProtoData used by other build tools.
