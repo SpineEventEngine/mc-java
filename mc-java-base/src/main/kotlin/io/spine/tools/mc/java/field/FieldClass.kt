820a2227cb29bf7dbd23862f1579d36ae9e00005
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-06-07 13:37:55 +0100

    Improve documentation

--- mc-java-base/src/main/kotlin/io/spine/tools/mc/java/field/FieldClass.kt
+++ mc-java-base/src/main/kotlin/io/spine/tools/mc/java/field/FieldClass.kt
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -54,7 +54,7 @@ public class FieldClass(
 
     public companion object {
         /**
-         * The name of the class generated Java class.
+         * The name of the generated Java class.
          */
         public const val NAME: String = "Field"
     }
