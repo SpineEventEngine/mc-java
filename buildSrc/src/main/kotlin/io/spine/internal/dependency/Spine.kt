9ce0d9e777ab4cb9d1b1c9190a937e7712761645
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-07-20 23:12:30 +0100

    Bump `tool-base` -> `2.0.0-SNAPSHOT.218`

--- buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
+++ buildSrc/src/main/kotlin/io/spine/internal/dependency/Spine.kt
@@ -124,7 +124,7 @@ object Spine {
          *
          * @see <a href="https://github.com/SpineEventEngine/tool-base">spine-tool-base</a>
          */
-        const val toolBase = "2.0.0-SNAPSHOT.217"
+        const val toolBase = "2.0.0-SNAPSHOT.218"
 
         /**
          * The version of [Spine.javadocTools].
