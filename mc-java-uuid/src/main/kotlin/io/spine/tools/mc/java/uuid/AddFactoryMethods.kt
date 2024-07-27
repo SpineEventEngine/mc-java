8e5ef362ce86fbf658bfe6a4af729a23ab2f74ae
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-07-27 01:07:31 +0100

    Fix code layout

--- mc-java-uuid/src/main/kotlin/io/spine/tools/mc/java/uuid/AddFactoryMethods.kt
+++ mc-java-uuid/src/main/kotlin/io/spine/tools/mc/java/uuid/AddFactoryMethods.kt
@@ -120,7 +120,8 @@ private class MethodOf(private val cls: PsiClass) {
              * @throws ${IllegalArgumentException::class.java.reference} 
              *          if the passed value is not a valid UUID string
              */
-            """.trimIndent())
+            """.trimIndent()
+        )
         doc
     }
 
