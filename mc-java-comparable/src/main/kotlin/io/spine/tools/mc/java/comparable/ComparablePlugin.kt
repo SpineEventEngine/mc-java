408bf3629f2ab7326c9e46a7a7d94a9d6706fca7
yevhenii-nadtochii <yevhenii.nadtochii@teamdev.com>
2024-08-27 12:54:04 +0200

    Implement `ComparableMessageDiscovery`

---
+++ mc-java-comparable/src/main/kotlin/io/spine/tools/mc/java/comparable/ComparablePlugin.kt
@@ -0,0 +1,55 @@
+/*
+ * Copyright 2024, TeamDev. All rights reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Redistribution and use in source and/or binary forms, with or without
+ * modification, must retain the above copyright notice and the following
+ * disclaimer.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+ * "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+ * LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+ * A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+ * OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+ * SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+ * LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+ * DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+ * THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package io.spine.tools.mc.java.comparable
+
+import io.spine.protodata.plugin.Plugin
+import io.spine.protodata.plugin.Policy
+import io.spine.protodata.plugin.View
+import io.spine.protodata.renderer.Renderer
+
+public class ComparablePlugin : Plugin {
+
+    override fun policies(): Set<Policy<*>> = setOf(
+        ComparableMessageDiscovery()
+    )
+
+    override fun views(): Set<Class<out View<*, *, *>>> = setOf(
+        UuidMessageView::class.java
+    )
+
+    override fun renderers(): List<Renderer<*>> = listOf(
+        UuidActionRenderer()
+    )
+
+    public companion object {
+
+        /**
+         * The ID for getting settings for the plugin.
+         */
+        public val SETTINGS_ID: String = ComparablePlugin::class.java.canonicalName
+    }
+}
