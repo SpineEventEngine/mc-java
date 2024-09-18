e4a40594575ab929332d18be7e54338c06e73cca
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2024-09-18 22:52:06 +0100

    Remove `mc-java-protoc` module

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -35,7 +35,6 @@ include(
     "mc-java-entity-tests",
     "mc-java-signal",
     "mc-java-signal-tests",
-//    "mc-java-protoc",
     "mc-java-marker",
     "mc-java-marker-tests",
     "mc-java-message-group",
