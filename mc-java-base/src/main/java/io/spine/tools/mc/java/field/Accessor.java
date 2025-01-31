287e42feb0380b1be6e235bee83b2bccf106133c
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-01-31 19:02:11 +0000

    Remove unused code

--- mc-java-base/src/main/java/io/spine/tools/mc/java/field/Accessor.java
+++ mc-java-base/src/main/java/io/spine/tools/mc/java/field/Accessor.java
@@ -1,11 +1,11 @@
 /*
- * Copyright 2022, TeamDev. All rights reserved.
+ * Copyright 2025, TeamDev. All rights reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- * http://www.apache.org/licenses/LICENSE-2.0
+ * https://www.apache.org/licenses/LICENSE-2.0
  *
  * Redistribution and use in source and/or binary forms, with or without
  * modification, must retain the above copyright notice and the following
@@ -28,7 +28,6 @@ package io.spine.tools.mc.java.field;
 
 import com.google.common.base.Objects;
 import com.google.errorprone.annotations.Immutable;
-import io.spine.tools.java.code.field.FieldName;
 
 import java.io.Serializable;
 
@@ -68,18 +67,6 @@ public final class Accessor implements Serializable {
         checkNotNull(prefix);
         checkNotNull(suffix);
         return new Accessor(prefix, suffix);
-    }
-
-    /**
-     * Formats an accessor method name based on this template and the given field name.
-     *
-     * @param field
-     *         the name of the field to access
-     * @return the method name
-     */
-    public String format(FieldName field) {
-        var name = String.format(template(), field.capitalize());
-        return name;
     }
 
     private String template() {
