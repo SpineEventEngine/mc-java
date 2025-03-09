d1b0c7aa168c1fcc439772f9c72918865a502d73
alexander-yevsyukov <alexander.yevsyukov@teamdev.com>
2025-03-09 17:29:12 +0000

    Restore commented assertion

--- mc-java-routing/src/test/kotlin/io/spine/tools/mc/java/routing/processor/KotlinRouteErrorSpec.kt
+++ mc-java-routing/src/test/kotlin/io/spine/tools/mc/java/routing/processor/KotlinRouteErrorSpec.kt
@@ -146,7 +146,7 @@ internal class KotlinRouteErrorSpec : RouteCompilationTest() {
             it shouldContain "`routeAgain(StatusReported, EventContext)`"
             it shouldContain "Please have only one function per routed message class."
             // The route for this event is not duplicated.
-//            it shouldNotContain "DeviceRegistered"
+            it shouldNotContain "DeviceRegistered"
         }
     }
 
