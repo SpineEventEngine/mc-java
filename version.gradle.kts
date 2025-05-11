02b527570263f4621f69d3d04ca8bd7a5b3273d7
Alexander Yevsyukov <alexander.yevsyukov@teamdev.com>
2025-05-11 18:31:09 +0100

    Bump version -> `2.0.0-SNAPSHOT.311`

--- version.gradle.kts
+++ version.gradle.kts
@@ -31,5 +31,5 @@
  *
  * For versions of Spine-based dependencies please see [io.spine.internal.dependency.spine].
  */
-val mcJavaVersion by extra("2.0.0-SNAPSHOT.310")
+val mcJavaVersion by extra("2.0.0-SNAPSHOT.311")
 val versionToPublish by extra(mcJavaVersion)
